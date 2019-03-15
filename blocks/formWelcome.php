aa62e83b0b1b034a99e43eb184f9b34f08712a79
Duong Le <leduong26298@gmail.com>
2019-03-16 04:10:39 +0700

    Re-design header in mobile view

--- blocks/formWelcome.php
+++
@@ -1,8 +0,0 @@
-<div class="row" style="height:22px;background-color:#ff3a3b;justify-content: flex-end">
-    <b style="color:white;  font-size: 13px;">
-        Welcome, <?php echo $_SESSION['Hoten'] ?>
-        <form name="Logout" method="POST" style="display: inline-block">
-            <button type="submit" name="btnLogout" class="logoutbtn" E><i class="fas fa-sign-out-alt"></i></button>
-        </form>
-    </b>
-</div> 
